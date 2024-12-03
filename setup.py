cbc857114e3753750a91e93bd0728843c1a29bbe
Pushkar-ingale <pushkaringale358@gmail.com>
2024-12-03 11:17:01 +0530

    Setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QA_SYSTEM',
+    version= '0.0.1',
+    author= 'Pushkar Ingale',
+    author_email= 'pushkaringale358@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
